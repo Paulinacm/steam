4d6e8d99b8c740278cba16656b26cf3c32bfa285
Paulinacm <pauly.cm@gmail.com>
2019-01-15 09:39:30 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,14 @@
+//manejo del DOM
+const resultTitleSteam = titleSteam(data);
+//console.log(resultTitleSteam)
+
+
+// Función que se ejecuta al cambiar filtro u orden
+function drawSteam() {
+  // Creo un array vacío, para recibir los array ordenados por las funciones
+  let arrSteamDraw = [];
+  // obtengo que opción de orden esta seleccionada
+  let optSort = document.getElementById("sort").value;
+  // obtengo que opción de filtro esta seleccionada
+  let optFilter = document.getElementById("filterType").value;
+}
