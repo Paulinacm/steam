e5f748665973def5faabaf7c83c6545847eb2592
Paulinacm <pauly.cm@gmail.com>
2019-01-16 11:20:12 -0300

    agregando require en data.spect

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,9 @@
+global.window = global;
+global.chai = require('chai');
+global.expect = require("chai").expect;
+global.assert = require("chai").assert;
+require('../src/data/steam/steam.js')
 require('../src/data.js');
-
 
 describe('example', () => {
   it('is a function', () => {
