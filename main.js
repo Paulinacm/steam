c1d05a8196e71644fce62a82cacea79b2c8ee958
Paulinacm <pauly.cm@gmail.com>
2019-01-16 09:06:51 -0300

    instalado chai

--- main.js
+++ main.js
@@ -6,12 +6,11 @@ let arrSteam =[];
 
 //arrFilter = data.filter(compare => (compare.type[0] === condition || compare.type[1] === condition));
 
-
 //ver titulo
 const resultTitleSteam = titleSteam(data);
 //console.log(resultTitleSteam)
 
-//en forma de boton, cambiarlo a select
+//ORDENAR en forma de boton, cambiarlo a select
 document.getElementById("author").addEventListener("click", (eventMouse) => {
     let dataOrdered = sortAuthorAz(data) 
     document.getElementById("allSteam").innerHTML =""
@@ -29,6 +28,54 @@ document.getElementById("author").addEventListener("click", (eventMouse) => {
         document.getElementById("allSteam").innerHTML += "<p>author: " + element.author+"</p>"
     });
 })
+
+
+// Función que se ejecuta al cambiar filtro u orden
+function drawSteamNews() {
+  // Creo un array vacío, para recibir los array ordenados por las funciones
+  let arrPokemonesDraw = [];
+  // obtengo que opción de orden esta seleccionada
+  let optSort = document.getElementById("sort").value;
+  // obtengo que opción de filtro esta seleccionada
+  let optFilter = document.getElementById("filterType").value;
+
+  // Otengo array ordenado para cada tipo de filtro
+  switch (optSort) {
+    case "nameaz":
+      arrPokemonesDraw = sortNameAz(arrPokemones);
+      break;
+    case "nameza":
+      arrPokemonesDraw = sortNameZa(arrPokemones);
+      break;
+    case "ascending":
+      arrPokemonesDraw = sortNumAsc(arrPokemones);
+      break;
+    case "descending":
+      arrPokemonesDraw = sortNumDesc(arrPokemones);
+      break;
+    default:
+      arrPokemonesDraw = arrPokemones;
+      break;
+  }
+
+  if (optFilter === "") {
+    // si no hay filtro seleccionado, utilizo array original de pokemones
+    arrPokemonesDraw = arrPokemones
+  } else {
+    // Si hay filtro seleccionado, utilizo la función para filtrar el array ya ordenado
+    arrPokemonesDraw = filterPokemon(arrPokemonesDraw, optFilter)
+  }
+
+  dataPokemon.innerHTML = ""
+  arrPokemonesDraw.forEach(element => {
+    const cardPokemon = createCard(element)
+    dataPokemon.appendChild(cardPokemon);
+
+  });
+  createGoogleChart(arrPokemonesDraw)
+}
+
+
 
 
 // ver titulo
