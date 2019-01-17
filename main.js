470df24e6cd7bbc9e2e8e8c4b24f656e633705f9
Paulinacm <pauly.cm@gmail.com>
2019-01-17 14:34:17 -0300

    incorporando función para crear card en main.js

--- main.js
+++ main.js
@@ -2,22 +2,81 @@
 // AQUI VA LA LOGICA. funciones filtrar, ordenar
 const data = STEAM.appnews.newsitems;
 
-const dataSteam = document.querySelector("#allSteam .showFilterSteam");
+let arrSteam = [];
 
-let arrSteam =[];
+//console.table(data)
+const containerRoot = document.getElementById('root');
+//console.log(containerRoot)
+const nameAuthor1 = document.getElementById('sortAuthor');
+
+const showData = (data) => {
+    let result = '';
+    //console.log(data)
+    data.forEach(element => {
+          //console.log(element.author);
+          if (element.author === '' && element.title === '') {
+            result = containerRoot.innerHTML += `
+            <div class = "col s6 m3">
+              <div class = "card">
+              <div class = "card-content">
+              <span class = "card-title activator grey-text text-darken-2"> 
+              <h6> $ {data[i].title} </h6>
+                <i class="material-icons right">more_vert</i > </span> </div> <div class = "card-reveal" >
+              <span class = "card-title grey-text text-darken-4" > ${data[i].author} 
+              <i class = "material-icons right"> close </i></span >
+              <p> Fecha: ${data[i].date} </p> 
+              <p> Game ID: ${data[i].gid} </p> 
+              <p> Feedlabel: ${data[i].feedlabel} </p> <span class = "link"
+            data - champion = '${data[i].url}' > Ver mas < /span> </div> 
+            </div> 
+              </div> ` 
+            }
+        }
+
+
+
+         nameAuthor1.addEventListener('change', () => {
+            let sortType = nameAuthor1.options[sortAuthor.selectedIndex].text;
+             //console.log(sortType)
+            let filtered = sortDataAuthor(data, sortType);
+            console.log(filtered)
+            // limpio div
+            containerRoot.innerHTML = '';
+
+            filtered.forEach(element => {
+              containerRoot.innerHTML += `
+
+    <div>
+      <div class="card">
+        <div class="box">
+          <div class="img">
+              <img src="${element.image}">
+          </div>
+          <h2>${element.name}<br><span>${element.house}</span></h2>
+          <p>Patronus: ${element.patronus}</p>
+        </div>
+      </div>
+    </div>`
+            })
+          })
+
+          window.onload = showData(data);
+        
+    
+
+//const dataSteam = document.querySelector("#allSteam .showFilterSteam");
 
 //arrFilter = data.filter(compare => (compare.type[0] === condition || compare.type[1] === condition));
 
 
 //ORDENAR cambiarlo a select
 const nameAuthor = document.getElementById('sortAuthor');
-let sortData = 
-
+let sortDataNews = 
     nameAuthor.addEventListener("change", () => {
         let sortType = nameAuthor.value;
-        console.log(sortType)
-        const arrTemp = sortData(data, sortType);
-        console.log(arrTemp)
+        //console.log(sortType)
+        const arrTemp = sortDataAuthor(data, sortType);
+        //console.log(arrTemp)
     });
 
 
@@ -56,6 +115,7 @@ document.getElementById("author").addEventListener("click", (eventMouse) => {
 })
 */
 
+/*
 // Función que se ejecuta al cambiar filtro u orden
 function drawSteamNews() {
   // Creo un array vacío, para recibir los array ordenados por las funciones
@@ -79,28 +139,28 @@ function drawSteamNews() {
     case "descending":
       arrSteamNewsDraw = sortNumDesc(arrSteam);
       break;*/
-    default:
-      arrSteamNewsDraw = arrSteam;
-      break;
-  }
+//     default:
+//       arrSteamNewsDraw = arrSteam;
+//       break;
+//   }
 
-  if (optFilter === "") {
-    // si no hay filtro seleccionado, utilizo array original de pokemones
-    arrSteamNewsDraw = arrSteam
-  } else {
-    // Si hay filtro seleccionado, utilizo la función para filtrar el array ya ordenado
-    arrSteamNewsDraw = filterSteam(arrSteamNewsDraw, optFilter)
-  }
+//   if (optFilter === "") {
+//     // si no hay filtro seleccionado, utilizo array original de pokemones
+//     arrSteamNewsDraw = arrSteam
+//   } else {
+//     // Si hay filtro seleccionado, utilizo la función para filtrar el array ya ordenado
+//     arrSteamNewsDraw = filterSteam(arrSteamNewsDraw, optFilter)
+//   }
 
-  dataSteam.innerHTML = ""
-  arrSteamNewsDraw.forEach(element => {
-    const cardPokemon = createCard(element)
-    dataSteam.appendChild(cardPokemon);
+//   dataSteam.innerHTML = ""
+//   arrSteamNewsDraw.forEach(element => {
+//     const cardPokemon = createCard(element)
+//     dataSteam.appendChild(cardPokemon);
 
-  });
-  //createGoogleChart(arrSteamNewsDraw)
-}
-
+//   });
+//   //createGoogleChart(arrSteamNewsDraw)
+// }
+// */
 
 
 ///////////////////
@@ -139,6 +199,6 @@ arrSteam.forEach(element => {
 });
 
 
-
-//document.getElementById('root').innerHTML = " ";
-//window.onload = loadData(title, date, author);
+/*document.getElementById('root').innerHTML = " ";
+window.onload = loadData(title, date, author)
+*/
